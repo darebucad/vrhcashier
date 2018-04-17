520d9d7dcf097ddeba74c5369dacdaf2fe377e0f
darebucad <bucad.markjulius@gmail.com>
2018-04-17 20:53:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+
+<?php
+
+/*
+    Index page of Dental Information System
+*/
+
+// Start Session
+session_start();
+
+// Database connection
+require __DIR__ . '/config/connection.php';
+$db = DB();
+
+// Application library ( with Library class )
+require __DIR__ . '/functions/library.php';
+$lib = new Library();
+
+$login_error_message = '';
+
+// check Login request
+if (!empty($_POST['btn_login'])) {
+
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    if (empty($username) || empty($password)) {
+      $login_error_message = 'Username and password must be filled out';
+    }
+    else{
+        // $user_id = $lib->Login($username, $password); // check user login
+
+
+        // if($user_id > 0){
+        if($username = 'admin' && $password='admin'){
+            $_SESSION['user_id'] = $user_id; // Set Session
+            header("location: src/dashboard.php"); // Redirect user to the profile.php
+        }
+        else{
+            $login_error_message = 'Incorrect username or password';
+        }
+    }
+  }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Login form</title>
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+</head>
+
+<body class="bg-dark">
+  <div class="container">
+    <div class="card card-login mx-auto mt-5">
+      <div class="card-header">Login</div>
+      <div class="card-body">
+        <form name="form_login" action="index.php" method="post">
+          <?php if ($login_error_message != "") {
+    echo '<strong>' . $login_error_message . '</strong>';
+    } ?>
+          <div class="form-group">
+            <label for="username-input">Username</label>
+            <input class="form-control" name="username" id="username-input" type="text" aria-describedby="emailHelp" placeholder="Username" autofocus>
+          </div>
+          <div class="form-group">
+            <label for="password-input">Password</label>
+            <input class="form-control" name="password" id="password-input" type="password" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <div class="form-check">
+              <label class="form-check-label">
+                <input class="form-check-input" type="checkbox"> Remember Password</label>
+            </div>
+          </div>
+          <input class="btn btn-primary btn-block" type="submit" name="btn_login" value="Login">
+        </form>
+        <div class="text-center">
+          <a class="d-block small mt-3" href="register.html">Register an Account</a>
+          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+</body>
+
+</html>
